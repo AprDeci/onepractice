474564c11015cc5fe4cfa4577705852761b4920c
aprdec <1263868407@qq.com>
2025-04-07 23:04:59 +0800

    token失效异常处理

--- src/main/java/top/aprdec/onepractice/eenum/CodeEnum.java
+++ src/main/java/top/aprdec/onepractice/eenum/CodeEnum.java
@@ -7,7 +7,8 @@ public enum CodeEnum {
     PASSWORD_ERROR(10001,"密码错误"),
     CAPTCHA_SEND_ERROR(10002,"验证码发送失败"),
     CAPTCHA_ERROR(10003,"验证码错误"),
-    USER_NOT_EXIST(10004,"用户不存在");
+    USER_NOT_EXIST(10004,"用户不存在"),
+    NOTLOGIN_ERROR(10005,"用户未登录");
 
 
     private int code;
--- src/main/java/top/aprdec/onepractice/exception/GloBalExceptionAdvice.java
+++ src/main/java/top/aprdec/onepractice/exception/GloBalExceptionAdvice.java
@@ -1,9 +1,11 @@
 package top.aprdec.onepractice.exception;
 
+import cn.dev33.satoken.exception.NotLoginException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import top.aprdec.onepractice.commmon.AResult;
+import top.aprdec.onepractice.eenum.CodeEnum;
 
 @Slf4j
 @RestControllerAdvice
@@ -12,6 +14,11 @@ public class GloBalExceptionAdvice {
     @ExceptionHandler(value = RuntimeException.class)
     public AResult<Object> handleException(Exception e) {
         return AResult.error(10001,e.getMessage());
+    }
+
+    @ExceptionHandler(value = NotLoginException.class)
+public AResult<Object> handleException(NotLoginException e) {
+    return AResult.error(CodeEnum.NOTLOGIN_ERROR.getCode(), e.getMessage());
     }
 
 }
