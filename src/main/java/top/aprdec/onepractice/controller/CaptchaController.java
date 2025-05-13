1a2e77adf2734fbbb29626bd931990f0cff35b73
aprdec <1263868407@qq.com>
2025-05-13 10:31:55 +0800

    验证码防抖

--- src/main/java/top/aprdec/onepractice/controller/CaptchaController.java
+++ src/main/java/top/aprdec/onepractice/controller/CaptchaController.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
+import top.aprdec.onepractice.Iinterface.Idempotent;
 import top.aprdec.onepractice.commmon.AResult;
 import top.aprdec.onepractice.dto.req.EmailCaptchaReqDTO;
 
@@ -16,6 +17,7 @@ import top.aprdec.onepractice.service.CaptchaService;
 public class CaptchaController {
     private final CaptchaService captchaService;
 
+    @Idempotent(timeout = 50,message = "验证码已发送，请稍后重试")
     @GetMapping("/email")
     public AResult getEmailCaptcha(@RequestParam @Email(message = "邮箱格式不正确") String email){
         Boolean result = captchaService.getEmailCaptcha(email);
