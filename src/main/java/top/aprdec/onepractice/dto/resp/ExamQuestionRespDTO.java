0a982f89d6e2030c68a083051fff59c6f70979ca
aprdec <1263868407@qq.com>
2025-05-13 10:31:44 +0800

    ExamQuestionRespDTO序列化

--- src/main/java/top/aprdec/onepractice/dto/resp/ExamQuestionRespDTO.java
+++ src/main/java/top/aprdec/onepractice/dto/resp/ExamQuestionRespDTO.java
@@ -3,8 +3,13 @@ package top.aprdec.onepractice.dto.resp;
 import lombok.Data;
 import top.aprdec.onepractice.dto.resp.subentity.QuestionPart;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class ExamQuestionRespDTO {
+public class ExamQuestionRespDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2784836381880029612L;
     private Integer paperId;
     private QuestionPart[] questionParts;
 }
