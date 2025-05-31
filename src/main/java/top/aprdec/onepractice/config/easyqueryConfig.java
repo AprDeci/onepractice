7402e3fd06973c42a8e34e3b2e2d4928737b818b
aprdec <1263868407@qq.com>
2025-05-31 11:55:22 +0800

    升级easyquery版本为3.0.8

--- src/main/java/top/aprdec/onepractice/config/easyqueryConfig.java
+++ src/main/java/top/aprdec/onepractice/config/easyqueryConfig.java
@@ -1,7 +1,6 @@
 package top.aprdec.onepractice.config;
 
 import com.easy.query.api.proxy.client.EasyEntityQuery;
-import com.easy.query.core.basic.api.database.CodeFirstExecutable;
 import com.easy.query.core.basic.api.database.DatabaseCodeFirst;
 import org.springframework.context.annotation.Configuration;
 import top.aprdec.onepractice.entity.UserDO;
